f004377df68eaf60bb0a288eee57cd091a98431f
Shalama <shalamafadillaa@gmail.com>
2026-07-21 10:02:14 +0700

    Set path and ejs configuration in app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require("node:path");
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-})
+// set ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(PORT, (error) => {
     if (error) {
