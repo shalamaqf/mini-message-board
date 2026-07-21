e14b04c9a37fc3fda0882a2d1df7ddd714fb9a8f
Shalama <shalamafadillaa@gmail.com>
2026-07-21 10:17:40 +0700

    Import indexRouter and set '/' path to it

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
+const indexRouter = require('./routes/indexRouter');
+
+app.use('/', indexRouter);
+
 app.listen(PORT, (error) => {
     if (error) {
         throw error;
