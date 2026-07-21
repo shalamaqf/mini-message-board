08bc3a4bc9ec22e6b7997afbdf60f5a1c53419f2
Shalama <shalamafadillaa@gmail.com>
2026-07-21 09:52:53 +0700

    Create an array that contains messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,2 +1,15 @@
 const { Router } = require('router');
 const indexRouter = Router();
+
+const messages = [
+    {
+        text: "Hi, there!",
+        user: "Amando",
+        date: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        date: new Date()
+    }
+]
