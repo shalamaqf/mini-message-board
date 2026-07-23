8ec7046404b10ea83f55c10eb858a82971811e9d
Shalama <shalamafadillaa@gmail.com>
2026-07-23 13:47:21 +0700

    Set path to assets static files

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require("node:path");
+const indexRouter = require('./routes/indexRouter');
+
 
 // set ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// set assets path
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 // app middleware to parse form data to req.body
 app.use(express.urlencoded({ extended: true }));
 
-
-const indexRouter = require('./routes/indexRouter');
 
 app.use('/', indexRouter);
 app.use((req, res, next) => {
