d41e1fefb125cc167bd59feab4ecfcdd9869fe39
Shalama <shalamafadillaa@gmail.com>
2026-07-23 13:13:43 +0700

    Create getMessageById function

---
+++ controller/messageController.js
@@ -0,0 +1,12 @@
+const messages = require('../db');
+
+function getMessageById(req, res) {
+    const targetId = Number(req.params.id);
+    const message = messages.find(message => message.id === targetId);
+
+    if (!message) {
+        return res.status(404).render('message-not-found', { title: `Message's Not Found`})
+    }
+
+    res.render('message-details', { message: message, title: `Message's Details`});
+}
