a449c961f2c04dfb701420daaa20656cdddda1ea
Shalama <shalamafadillaa@gmail.com>
2026-08-03 14:47:00 +0700

    Create main function
    Create it to execute queries into the database.

--- db/populatedb.js
+++ db/populatedb.js
@@ -13,3 +13,19 @@ const SQL = `
         ('Hi, there!', 'Amando', CURRENT_DATE),
         ('Hello World!', 'Charles', CURRENT_DATE);
 `;
+
+// load env variables
+require("dotenv").config();
+
+async function main() {
+    console.log("seeding...");
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+}
+
+main();
